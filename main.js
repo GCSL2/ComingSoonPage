5e48f3e6e30517f8fae232fe9baaaa6db5829146
Anderton Rodrigues <73655184+AnderRedna@users.noreply.github.com>
2023-05-22 19:07:02 -0300

    responsive design

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ function setVh() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
+
+// Execute a função uma vez quando a página é carregada
+setVh();
+
+// Execute a função novamente sempre que a janela é redimensionada
+window.addEventListener('resize', setVh);
+
+  
